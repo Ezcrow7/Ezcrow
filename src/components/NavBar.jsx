6b7256b6bf8ba2e43082d9f3325b5398037c6f9b
jorniks <dictforum@gmail.com>
2023-10-18 20:02:58 +0100

    created nav bar and its contents

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+
+const NavBar = () => {
+  return (
+    <div className="w-full px-5 bg-transparent border-b">
+      <div className="w-full max-w-[77rem] mx-auto py-2 flex justify-between items-center">
+        <a href="/" className="">
+          <img src="/assets/logo/ezcrow-logo.png" alt="Ezcrow Logo" className="h-10 sm:h-[3.2rem]" />
+        </a>
+
+        <button className="px-4 py-2 border-2 border-ezcrow-950 text-sm">Connect Wallet</button>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
