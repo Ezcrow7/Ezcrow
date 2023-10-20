a46bc9ada081d56b9cd1597e7e8c70f503e69b72
jorniks <dictforum@gmail.com>
2023-10-20 06:06:36 +0100

    update with default color for app and style configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import NavBar from "./components/NavBar";
+import Listings from "./pages/Listings";
 
 
 function App() {
   return (
     <div className="">
+      <NavBar />
+
+      <Listings />
     </div>
   );
 }
