ed8edb745c028d8f26c447b0260155a08bb6c486
jorniks <dictforum@gmail.com>
2023-10-22 13:24:05 +0100

    update to listings page

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,6 +1,12 @@
 import {useState} from 'react'
 import BuyListings from '../components/BuyListings'
 import SellListings from '../components/SellListings'
+import Filter from '../components/Filter'
+
+
+
+
+
 
 const Listings = () => {
   const [tabToShow, setTabToShow] = useState("buy")
@@ -13,26 +19,24 @@ const Listings = () => {
 
 
 
-
-
-
   return (
     <div className="">
-      <section className="border-b pt-8 pb-2 shadow sticky top-0">
-        <div className="app-container">
-          <div className={`grid grid-cols-12 items-center w-44 text-center p-0.5 text-sm font-semibold transition-all duration-500 border-2 ${tabToShow === 'sell' ? "border-red-400" : "border-ezcrow-950"}`}>
-            <h3 className={`col-span-6 cursor-pointer px-3 py-2.5 transition-all duration-500 ${tabToShow === 'buy' && "bg-ezcrow-950 text-white"}`}
-              onClick={() => setTabToShow("buy")}
-            >Buy</h3>
-            <h3 className={`col-span-6 cursor-pointer px-3 py-2.5 transition-all duration-500 ${tabToShow === 'sell' && "bg-red-400 text-white"}`}
-              onClick={() => setTabToShow("sell")}
-            >Sell</h3>
+      <section className="pt-8 sticky top-0 space-y-4">
+        <div className="border-b pb-2 shadow">
+          <div className="app-container">
+            <div className={`grid grid-cols-12 items-center w-44 text-center p-0.5 text-sm font-semibold transition-all duration-500 border-2 ${tabToShow === 'sell' ? "border-red-400" : "border-ezcrow-950"}`}>
+              <h3 className={`col-span-6 cursor-pointer px-3 py-2.5 transition-all duration-500 ${tabToShow === 'buy' && "bg-ezcrow-950 text-white"}`}
+                onClick={() => setTabToShow("buy")}
+              >Buy</h3>
+              <h3 className={`col-span-6 cursor-pointer px-3 py-2.5 transition-all duration-500 ${tabToShow === 'sell' && "bg-red-400 text-white"}`}
+                onClick={() => setTabToShow("sell")}
+              >Sell</h3>
+            </div>
           </div>
         </div>
         
         {/* START OF FILTER COMPONENTS */}
-        <div className="">
-        </div>
+        <Filter />
         {/* START OF FILTER COMPONENTS */}
       </section>
 
