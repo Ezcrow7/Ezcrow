595555bf312af0d813d3ecf1cd94caefc12fa5a5
jorniks <dictforum@gmail.com>
2023-10-24 19:29:24 +0100

    created web3 read and write files and wrote in some of their functions

---
+++ src/appCore/web3ReadFunctions.js
@@ -0,0 +1,57 @@
+import { GENERAL_TRADE_ABI, SELL_TOKEN_ABI, dINR_TRADE_ADDRESS } from "./coreVariables"
+import { getConnectedWalletAddress, initContract } from "./globalFunctions"
+
+
+
+
+
+
+
+
+
+export const isSpendApproved = async (tokenAddress, tradeAddress) => {
+  try {
+    const account = await getConnectedWalletAddress()
+    const contract = await initContract(SELL_TOKEN_ABI, tokenAddress)
+    const approvedAmount = await contract.methods.allowance(account, tradeAddress).call()
+
+    // console.log('approvedAmount', approvedAmount);
+
+    return approvedAmount
+  } catch (isSpendApprovedError) {
+    console.info('isSpendApprovedError: ', isSpendApprovedError.message);
+    return 0
+  }
+}
+
+
+
+export const getOfferedTokens = async () => {
+  try {
+    const eventContract = await initContract(GENERAL_TRADE_ABI, dINR_TRADE_ADDRESS),
+          offeredToken = await eventContract.methods.getAllEventOffers('EzcrowDemo').call();
+
+    // console.log('offeredToken', offeredToken);
+    return offeredToken;
+  } catch (getOfferedTokensError) {
+    console.info('getOfferedTokensError: ', getOfferedTokensError.message);
+    return false
+  }
+}
+
+
+
+export const isTradeSaleOn = async () => {
+  try {
+    const eventContract = await initContract(GENERAL_TRADE_ABI, dINR_TRADE_ADDRESS),
+          tradeSales = await eventContract.methods.isSaleOn('EzcrowDemo').call();
+
+    // console.info(eventCode, tradeSales.saleActive);
+    return {
+      tradeSaleActive: tradeSales.saleActive
+    };
+  } catch (isTradeSaleOnError) {
+    console.info('isTradeSaleOnError: ', isTradeSaleOnError.message);
+    // return false
+  }
+}
---
+++ src/appCore/web3WriteFunctions.js
@@ -0,0 +1,84 @@
+import { GENERAL_TRADE_ABI, SELL_TOKEN_ABI } from "./coreVariables";
+import { getConnectedWalletAddress, initContract, web3 } from "./globalFunctions";
+
+
+
+
+
+export const approveStandardTokenToSpendOnTrade = async (tokenBuyAmount) => {
+  let account = await getConnectedWalletAddress(),
+      teamTokenContract = await initContract(SELL_TOKEN_ABI, RBN_STANDARD_TOKEN_ADDRESS),
+      approveAmount = web3.utils.toWei(tokenBuyAmount.toString(), 'lovelace');
+
+
+
+  return new Promise((resolve, reject) => {
+    teamTokenContract.methods.approve(RBN_TRADE_TOKEN_ADDRESS, approveAmount)
+    .send({
+      from: account,
+      maxPriorityFeePerGas: null,
+      maxFeePerGas: null,
+    })
+    .once("transactionHash", (hash) => {
+      toast.info("Your approve spend transaction is pending confirmation on the block");
+      console.log("transactionHash", hash);
+    })
+    .on('receipt', (receipt) => {
+      toast.success('Approve spend transaction successfully mined!')
+      console.info('Transaction Receipt', receipt);
+
+      return resolve(true);
+    })
+    .on('error', (approveSpendCatchError) => {
+      toast.error(approveSpendCatchError.message)
+      // console.info('approveSpendCatchError', approveSpendCatchError.message)
+      return reject(false);
+    })
+  }).catch((approveSpendError) => {
+    toast.error(approveSpendError.message)
+    console.info('approveSpendError', approveSpendError.message)
+    return false
+  });
+}
+
+
+
+
+export const buyOfferedTradeToken = async (eventCode, offerId, amountToBuy, tradeTokenAddress) => {
+  const userAddress = await getConnectedWalletAddress(),
+        mainContract = await initContract(GENERAL_TRADE_ABI, tradeTokenAddress),
+        convertedAmountToBuy = web3.utils.toWei(amountToBuy.toString(), 'lovelace')
+
+
+  return new Promise((resolve, reject) => {
+    mainContract.methods.buyOfferedTokens(eventCode, offerId, convertedAmountToBuy)
+    .send({
+      from: userAddress
+    })
+    .once('transactionHash', (hash) => {
+      console.log("hash", hash);
+
+      toast.info("Your transaction is pending confirmation on the block");
+    })
+    .once('receipt', (receipt) => {
+      console.info(receipt);
+      toast.success("Your transaction receipt is ready! ");
+    })
+    .once('confirmation', (confirmed) => {
+      console.info("confirmed", confirmed);
+      toast.success("Your transaction is confirmed! ");
+
+      return resolve(true);
+    })
+    .on('error', (contractError) => {
+      toast.error(contractError.message)
+      console.log(contractError);
+
+      return reject(false)
+    })
+  }).catch(promiseError => {
+    console.log(promiseError);
+    toast.error(promiseError.message)
+    return false;
+  })
+}
