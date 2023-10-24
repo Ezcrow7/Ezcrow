7935a4a9f93013ab5397edf32879a22df1569996
jorniks <dictforum@gmail.com>
2023-10-24 19:34:00 +0100

    updated listings file to make listed tokens dynamic and to not display tokens with 0 balance

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,15 +1,35 @@
-import {useState} from 'react'
+import { useEffect, useState } from 'react'
+import { getOfferedTokens } from '../appCore/web3ReadFunctions'
 import BuyListings from '../components/BuyListings'
 import SellListings from '../components/SellListings'
 import Filter from '../components/Filter'
+import { tokenPairs } from '../appCore/coreVariables'
 
 
 
 
 
 
-const Listings = () => {
+const Listings = ({ switchedNetwork }) => {
   const [tabToShow, setTabToShow] = useState("buy")
+  const [offeredTokens, setOfferedTokens] = useState([])
+
+
+  useEffect(() => {
+    if (switchedNetwork.status) {
+      getOfferedTokens().then(tokens => {
+        const updatedArray = tokens?.map((obj) => {
+          return tokenPairs?.map((tokenPair) => ({
+            ...obj,
+            ...tokenPair
+          }))
+        })
+
+        // console.log('updatedArray', updatedArray.flat());
+        setOfferedTokens(updatedArray.flat())
+      })
+    }
+  }, [switchedNetwork.status])
 
 
 
@@ -21,14 +41,14 @@ const Listings = () => {
 
   return (
     <div className="">
-      <section className="pt-8 sticky top-0 space-y-4">
+      <section className="pt-6 sticky top-0 space-y-4 bg-white">
         <div className="border-b pb-2 shadow">
           <div className="app-container">
             <div className={`grid grid-cols-12 items-center w-44 text-center p-0.5 text-sm font-semibold transition-all duration-500 border-2 ${tabToShow === 'sell' ? "border-red-400" : "border-ezcrow-950"}`}>
-              <h3 className={`col-span-6 cursor-pointer px-3 py-2.5 transition-all duration-500 ${tabToShow === 'buy' && "bg-ezcrow-950 text-white"}`}
+              <h3 className={`col-span-6 cursor-pointer px-3 py-2 transition-all duration-500 ${tabToShow === 'buy' && "bg-ezcrow-950 text-white"}`}
                 onClick={() => setTabToShow("buy")}
               >Buy</h3>
-              <h3 className={`col-span-6 cursor-pointer px-3 py-2.5 transition-all duration-500 ${tabToShow === 'sell' && "bg-red-400 text-white"}`}
+              <h3 className={`col-span-6 cursor-pointer px-3 py-2 transition-all duration-500 ${tabToShow === 'sell' && "bg-red-400 text-white"}`}
                 onClick={() => setTabToShow("sell")}
               >Sell</h3>
             </div>
@@ -59,10 +79,20 @@ const Listings = () => {
           </aside>
         </section>
         
-        { tabToShow === 'buy' ?
-          <BuyListings />
-        :
-          <SellListings />
+        {offeredTokens.map((offeredToken, index) => (
+          offeredToken?.tokensLeft / Math.pow(10, 6) > 0 &&
+          <div key={index} className="">
+            {tabToShow === 'buy' ?
+              <BuyListings
+                offeredToken={offeredToken}
+              />
+            :
+              <SellListings
+                offeredToken={offeredToken}
+              />
+            }
+          </div>
+        ))
         }
       </section>
     </div>
